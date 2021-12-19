4264f30f74613f1796f3676e62035a2407e10de1
Gerard Hynes <gkhynes@gmail.com>
2021-12-19 11:44:05 +0000

    Add FeedbackList, FeedbackItem and Card components

---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Card from "./Card.svelte"
+  export let item
+</script>
+
+<Card>
+  <div class="num-display">
+    {item.rating}
+  </div>
+  <div class="close">
+    X
+  </div>
+  <p class="text-display">
+    {item.text}
+  </p>
+</Card>
